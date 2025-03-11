c45f013fe083b11d86fea313e46ceb167eb4ef5e
Daniel-1110 <rl244100026@uma.sv>
2025-03-10 19:48:54 -0600

    segundo commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { sequelize } from "./config/database.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+import studentRoutes from './routes/students.js';
+app.use('/students', studentRoutes);
+
+const PORT = 3000;
+
+sequelize.sync()
+   .then(()=> {
+      console.log('Base de Datos sincronizada');
+      app.listen(PORT, (res) => {
+         try {
+            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+            
+         }
+         catch (error) {
+            console.error("No se pudo iniciar el servidor: ", error);
+         }
+      });
+   })
+      .catch(err=>{console.error('Error al establecer la conexión con la base de datos', err)});
